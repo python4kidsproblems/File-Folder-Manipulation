07442039fdf0433bb608a63d52edc264063fd1ed
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-09-02 01:36:15 +0500

    Add files via upload

---
+++ file_folder_manipulation.py
@@ -0,0 +1,67 @@
+# Classes Inheritance Practice Problems
+
+# How to use this file:
+#  Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# There is a jpg file called file_folder.jpg in this folder at link:     #
+# https://github.com/python4kidsproblems/File-Folder-Manipulation/blob/  #
+# master/file_folder.jpg. Use only OS and Shutil commands to the         #
+# directory. Files 1-9 should be simple text files.                      #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Now using file I/O, write the following in the files:                  #
+# File 1: “In our kitchen, however, they are faced with something so out #
+# of proportion to their experience that they cannot deal with it. ”     #
+# File 2: “Although we are pleased, we are also upset, because the mice  #
+# behave as though there were something wrong with our kitchen. ”        #
+# File 3: “There is more food lying about in our kitchen, more crumbs on #
+# the counters and filthy scraps of onion kicked against the base of the #
+# cabinets. In fact, there is so much loose food in the kitchen I can    #
+# only think the mice themselves are defeated by it. ”                   #
+# File 4: “uncomfortable and embarrassed at not being able to scavenge   #
+# as they should.”                                                       #
+# File 5: “In a tidy kitchen, it is a challenge for them to find enough  #
+# food night after night to survive until spring. ”                      #
+# File 6: “Mice live in our walls but do not trouble our kitchen. We are #
+# pleased but cannot understand why they do not come into our kitchen    #
+# where we have traps set, as they come into the kitchens of our         #
+# neighbors. ”                                                           #
+# File 7: “They might venture out a few steps, but soon the overwhelming #
+# sights and smells drive them back into their holes, ”                  #
+# File 8: “What makes this even more puzzling is that our house is much  #
+# less tidy than the houses of our neighbors. ”                          #
+# File 9: “They patiently hunt and nibble hour after hour until they are #
+# satisfied. ”                                                           #
+# Here’s the complete story, “The Mice” by Lydia Davis:                  #
+# Mice live in our walls but do not trouble our kitchen. We are pleased  #
+# but cannot understand why they do not come into our kitchen where we   #
+# have traps set, as they come into the kitchens of our neighbors.       #
+# Although we are pleased, we are also upset, because the mice behave as #
+# though there were something wrong with our kitchen. What makes this    #
+# even more puzzling is that our house is much less tidy than the houses #
+# of our neighbors. There is more food lying about in our kitchen, more  #
+# crumbs on the counters and filthy scraps of onion kicked against the   #
+# base of the cabinets. In fact, there is so much loose food in the      #
+# kitchen I can only think the mice themselves are defeated by it. In a  #
+# tidy kitchen, it is a challenge for them to find enough food night     #
+# after night to survive until spring. They patiently hunt and nibble    #
+# hour after hour until they are satisfied. In our kitchen, however,     #
+# they are faced with something so out of proportion to their experience #
+# that they cannot deal with it. They might venture out a few steps, but #
+# soon the overwhelming sights and smells drive them back into their     #
+# holes, uncomfortable and embarrassed at not being able to scavenge as  #
+# they should.                                                           #
+##########################################################################
+
+# your code goes here
+
+################################### Q3 ###################################
+# Using only File I/O read the files in the correct order and compile    #
+# the complete story in a new text file called “File 10”.                #
+##########################################################################
+
+# your code goes here
